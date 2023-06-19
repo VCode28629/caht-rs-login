1a21931301328d04bcc564b07292eec86c9ce9e4
VCode <vcode28629@qq.com>
2023-06-19 20:00:16 +0800

    chinese support

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 use iced::{
     executor,
-    futures::{
-        channel::mpsc::{channel, Receiver, Sender},
-        SinkExt,
-    },
     widget::{button, column, row, text, text_input},
-    Alignment, Application, Command, Executor, Sandbox, Settings, Theme,
+    window, Alignment, Application, Command, Settings, Theme, Color, Element,
 };
-use std::{io, thread};
-// use std::sync::mpsc::channel;
+use std::io;
 
 #[derive(Default)]
 pub struct Login {
@@ -38,6 +33,11 @@ impl Application for Login {
     type Flags = ();
     fn new(_flags: ()) -> (Self, Command<Self::Message>) {
         (
+            // Login {
+            //     username: String::new(),
+            //     password: String::new(),
+            //     hint: String::from("aaaavbsdvdsa"),
+            // },
             Default::default(),
             Command::perform(recieve_message(), |s| LoginMessage::RecievedMessage(s)),
         )
@@ -61,14 +61,14 @@ impl Application for Login {
                 } else if self.password.len() > 32 {
                     self.hint = "Password too long".to_string();
                 } else {
-                    print!("Login\n{}\n{}", self.username,self.password);
+                    print!("Login\n{}\n{}\n\n", self.username, self.password);
                 }
             }
             Self::Message::SignUp => {
-                print!("SignUp\n{}\n{}", self.username,self.password);
+                print!("SignUp\n{}\n{}\n\n", self.username, self.password);
             }
             Self::Message::RecievedMessage(s) => {
-                println!("recieved: {}", s);
+                print!("recieved: {}\n\n", s);
             }
         }
         Command::perform(recieve_message(), |s| LoginMessage::RecievedMessage(s))
@@ -80,28 +80,37 @@ impl Application for Login {
         let password = text_input("password", &self.password)
             .on_input(Self::Message::PasswordChanged)
             .password();
-        let hint = text("");
-        let login_button = button("Login").on_press(Self::Message::Login);
-        let sign_up_button = button("Sign Up").on_press(Self::Message::SignUp);
-
-        column![
-            username,
-            password,
+        let hint = text(&self.hint);
+        let login_button = button("Login").on_press(Self::Message::Login).height(38);
+        let sign_up_button = button("Sign Up").on_press(Self::Message::SignUp).height(38);
+        let content: Element<_> = column![
+            column![username, password].spacing(10),
             hint,
             row![login_button, sign_up_button,]
                 .spacing(10)
-                .padding(20)
+                .padding(10)
                 .align_items(Alignment::Center)
         ]
-        .spacing(10)
+        // .spacing(10)
         .padding(20)
         .align_items(Alignment::Center)
-        .into()
+        .into();
+        content
+        // .explain(Color::BLACK)
     }
 
     type Theme = Theme;
 }
 
 fn main() -> iced::Result {
-    Login::run(Settings::default())
+    // let font = font::load();
+    Login::run(Settings {
+        window: window::Settings {
+            size: (320, 180),
+            resizable: false,
+            ..Default::default()
+        },
+        default_font: Some(include_bytes!("../resources/fonts/simsun.ttc")),
+        ..Default::default()
+    })
 }
